b0f4c228b18eb55bbc0b82b25f077b8de7c79e0e
Magnus A <71440734+CyclistHomeCoder@users.noreply.github.com>
2020-09-26 11:04:28 +0200

    Added dir_name param and aligned default values

--- PIcFrame_config.py
+++ PIcFrame_config.py
@@ -1,11 +1,9 @@
 
-"""
+""""
 This is the config file used by PictureFrame. It allows settings to be changed
 on the command line when the program is run, or you can alter the default values
 here
-
 """
-
 import argparse
 
 """
@@ -22,9 +20,9 @@ def str_to_tuple(x):
 # NB the reason that absolute paths are used here is because relative ones can lead
 #  to abiguity if the program is started automatically on boot.
 parse = argparse.ArgumentParser("start running a picture frame")
-parse.add_argument("-a", "--blur_amount",   default=8, type=float, help="larger values than 12 will increase processing load quite a bit")
-parse.add_argument("-b", "--blur_edges",    default=True, type=str_to_bool, help="use blurred version of image to fill edges - will override FIT = False")
-parse.add_argument("-c", "--check_dir_tm",  default=120.0, type=float, help="time in seconds between checking if the image directory has changed")
+parse.add_argument("-a", "--blur_amount",   default=12, type=float, help="larger values than 12 will increase processing load quite a bit")
+parse.add_argument("-b", "--blur_edges",    default=False, type=str_to_bool, help="use blurred version of image to fill edges - will override FIT = False")
+parse.add_argument("-c", "--check_dir_tm",  default=60.0, type=float, help="time in seconds between checking if the image directory has changed")
 parse.add_argument("-d", "--verbose",       default=False, type=str_to_bool, help="show try/exception messages")
 parse.add_argument("-e", "--edge_alpha",    default=0.5, type=float, help="background colour at edge. 1.0 would show reflection of image")
 parse.add_argument("-f", "--fps",           default=20.0, type=float)
@@ -43,15 +41,16 @@ parse.add_argument("-o", "--font_file",     default="/home/pi/PIcFrame/fonts/Not
 parse.add_argument("-p", "--pic_dir",       default="/home/pi/Pictures")
 parse.add_argument("-q", "--shader",        default="/home/pi/PIcFrame/shaders/blend_new")
 parse.add_argument("-r", "--reshuffle_num", default=1, type=int, help="times through before reshuffling")
-parse.add_argument("-s", "--show_names_tm", default=0, type=float, help="time to show text over image with file name")
-parse.add_argument(      "--skip_dir",      default="@eaDir", help="skip directories with this name")
-parse.add_argument("-t", "--fit",           default=True, type=str_to_bool, help="shrink to fit screen i.e. don't crop")
+parse.add_argument("-s", "--show_names_tm", default=10.0, type=float, help="time to show text over image with file name")
+parse.add_argument(      "--use_dir_name",  default=False, type=str_to_bool, help="use directory name instead of file name, need show_name_tm > 0")
+parse.add_argument(      "--skip_dir",      default="", help="skip directories with this name")
+parse.add_argument("-t", "--fit",           default=False, type=str_to_bool, help="shrink to fit screen i.e. don't crop")
 parse.add_argument("-u", "--kenburns",      default=False, type=str_to_bool, help="will set FIT->False and BLUR_EDGES->False")
-parse.add_argument("-v", "--time_delay",    default=20.0, type=float, help="time between consecutive slide starts - can be changed by MQTT")
+parse.add_argument("-v", "--time_delay",    default=30.0, type=float, help="time between consecutive slide starts - can be changed by MQTT")
 parse.add_argument("-w", "--fade_time",     default=4.0, type=float, help="change time during which slides overlap - can be changed by MQTT")
 parse.add_argument("-x", "--shuffle",       default=True, type=str_to_bool, help="shuffle on reloading image files - can be changed by MQTT")
 parse.add_argument("-y", "--subdirectory",  default="", help="subdir of pic_dir - can be changed by MQTT")
-parse.add_argument("-z", "--blur_zoom",     default=2.0, type=float, help="must be >= 1.0 which expands the backgorund to just fill the space around the image")
+parse.add_argument("-z", "--blur_zoom",     default=1.0, type=float, help="must be >= 1.0 which expands the backgorund to just fill the space around the image")
 parse.add_argument(      "--auto_resize",   default=True, type=str_to_bool, help="set this to false if you want to use 4K resolution on Raspberry Pi 4. You should ensure your images are the correct size for the display")
 parse.add_argument(      "--delay_exif",    default=True, type=str_to_bool, help="set this to false if there are problems with date filtering - it will take a long time for initial loading if there are many images.")
 args = parse.parse_args()
@@ -90,7 +89,8 @@ SUBDIRECTORY = args.subdirectory
 BLUR_ZOOM = args.blur_zoom
 MIN_SIZE = args.min_size
 SKIP_DIR = args.skip_dir
+USE_DIR_NAME=args.use_dir_name
 AUTO_RESIZE = args.auto_resize
 DELAY_EXIF = args.delay_exif
 
-CODEPOINTS = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ., _-/' # limit to 49 ie 7x7 grid_size
+CODEPOINTS = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ., _-/ ' # limit to 49 ie 7x7 grid_size
